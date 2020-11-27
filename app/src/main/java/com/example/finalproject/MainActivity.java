fbb9db8cf58ba2f30418b9bc670395dbf98768e8
Enrike568 <moment04@inbox.ru>
2020-11-28 00:29:19 +0300

    -radiobutton

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,15 +17,31 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.text.BreakIterator;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
+
+    Button btnActTwo;
+
     private Object Text;
     private Button encrypt;
     private Object Code;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnActTwo = (Button) findViewById(R.id.btnActTwo);
+        btnActTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+
+            }
+        });
+
 
         final EditText etInput = (EditText) findViewById(R.id.etInput);
         final TextView tvOutput = (TextView) findViewById(R.id.tvOutput);
@@ -38,16 +55,18 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public static String md5(final String s) { final String MD5 = "MD5"; try {
+    public static String md5(final String s) {
+        final String MD5 = "MD5";
+        try {
         MessageDigest digest = java.security.MessageDigest .getInstance(MD5);
-        ((MessageDigest) digest).update(s.getBytes());
-        byte messageDigest[] = digest.digest();
-        StringBuilder hexString = new StringBuilder();
-        for (byte aMessageDigest : messageDigest) { String h = Integer.toHexString(0xFF & aMessageDigest);
+            ((MessageDigest) digest).update(s.getBytes());
+                byte messageDigest[] = digest.digest();
+                    StringBuilder hexString = new StringBuilder();
+                for (byte aMessageDigest : messageDigest) { String h = Integer.toHexString(0xFF & aMessageDigest);
             while (h.length() < 2) h = "0" + h; hexString.append(h);
         } return hexString.toString();
     } catch (NoSuchAlgorithmException e) { e.printStackTrace();
-    } return "";
+        } return "";
     }
 }
 
--- app/src/main/java/com/example/finalproject/MainActivity2.java
+++ app/src/main/java/com/example/finalproject/MainActivity2.java
@@ -1,40 +1,141 @@
 package com.example.finalproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
-import java.math.BigInteger;
+import androidx.appcompat.app.AppCompatActivity;
 
-import static com.example.finalproject.R.id.etInput;
-import static com.example.finalproject.R.id.radio;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    Button button;
+    String sha;
+    RadioGroup radioGroup;
+    RadioButton sha1;
+    RadioButton sha224;
+    RadioButton sha256;
+    RadioButton sha384;
+    RadioButton sha512;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        radioGroup = findViewById(R.id.radiogroup);
+
+        final EditText etInput = (EditText) findViewById(R.id.etInput);
+        final TextView tvOutput = (TextView) findViewById(R.id.tvOutput);
+
+        sha1 = findViewById(R.id.sha1);
+        sha224 = findViewById(R.id.sha224);
+        sha256 = findViewById(R.id.sha256);
+        sha512 = findViewById(R.id.sha512);
+        sha384 = findViewById(R.id.sha384);
+
+        button = findViewById(R.id.button);
+
+        int rgid;
+
+        final String input = etInput.getText().toString();
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                rgid[0] = checkedId;
+
+            }
+        });
+
+        switch (rgid[0]){
+
+            case R.id.sha1:
+                sha = "SHA-1";
+                break;
+
+            case R.id.sha224:
+                sha = "SHA-224";
+                break;
+
+            case R.id.sha256:
+                sha = "SHA-256";
+                break;
+
+            case R.id.sha512:
+                sha = "SHA-512";
+                break;
+            case R.id.sha384:
+                sha = "SHA-384";
+                break;
+            default:
+                sha = "SHA-1";
+                break;
+
+        }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.d("TAGA", "1");
+                Log.d("TAGA", sha);
+                tvOutput.setText(encryptThisString(input, sha));
+
+                Log.d("TAGA", tvOutput.getText().toString());
+
+
+            }
+
+
+        });
     }
 
-//TODO: SHA-1,SHA-224,SHA-256,SHA-384,SHA-512
-
-    public void btnSHA(View view) {
-        EditText etInput = (EditText) findViewById(R.id.etInput);
-        TextView tvOutput = (TextView) findViewById(R.id.tvOutput);
-
-        byte[] inputData = etInput.getText().toString().getBytes();
-        byte[] outputData = new byte[0];
+    public static String encryptThisString(String input, String sha) {
 
         try {
-            outputData = sha.encryptSHA(inputData, "SHA-1");
-        } catch (Exception e) {
-            e.printStackTrace();
+
+            MessageDigest md = MessageDigest.getInstance(sha);
+
+            byte[] messageDigest = md.digest(input.getBytes());
+
+            BigInteger no = new BigInteger(1, messageDigest);
+
+            String hashtext = no.toString(16);
+
+            while (hashtext.length() < 32) {
+
+                hashtext = "0" + hashtext;
+
+            }
+
+            return hashtext;
+
         }
+
+        catch (NoSuchAlgorithmException e) {
+
+            throw new RuntimeException(e);
+
+        }
+
     }
+
 }
+
 
 
